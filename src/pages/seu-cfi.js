5e419fb4f16e579db6f98eb611cc4931dd54dfbb
tommy martinez <thomasjohnmartinez@gmail.com>
2023-08-07 16:43:34 -0400

    added american project

--- src/pages/seu-cfi.js
+++ src/pages/seu-cfi.js
@@ -9,7 +9,7 @@ function Collapse(props) {
         <h4 className="m-0 not-prose" {...getToggleProps({
           onClick: () => setExpanded((prevExpanded) => !prevExpanded),
         })}>
-            {isExpanded ? 'Augmented reality application Mindy Seu\'s: Cyberfeminism Index (2022)' : 'Augmented reality application Mindy Seu\'s: Cyberfeminism Index (2022)'}
+            {isExpanded ? 'Augmented reality application for Mindy Seu\'s: Cyberfeminism Index (2022)' : 'Augmented reality application for Mindy Seu\'s: Cyberfeminism Index (2022)'}
         </h4>
         <div {...getCollapseProps()}>
             <div className="my-6">
@@ -21,16 +21,17 @@ function Collapse(props) {
   }
 const CFI = () =>{
   return (
-   <>
-   <Collapse>
-      <figure>
-        <img className="photo" src={require('../images/CFI.webp')} alt="" />  
-      </figure>
-      <p>In Cyberfeminism Index, hackers, scholars, artists, and activists of all regions, races and sexual orientations consider how humans might reconstruct themselves by way of technology. When learning about internet history, we are taught to focus on engineering, the military-industrial complex, and the grandfathers who created the architecture and protocol, but the internet is not only a network of cables, servers, and computers. It is an environment that shapes and is shaped by its inhabitants and their use.</p>   
-      <p>The creation and use of the Cyberfeminism Index is a social and political act. It takes the name cyberfeminism as an umbrella, complicates it, and pushes it into plain sight. Edited by designer, professor, and researcher Mindy Seu, it includes more than 700 short entries of radical techno-critical activism in a variety of media, including excerpts from academic articles and scholarly texts; descriptions of hackerspaces, digital rights activist groups, and bio-hacktivism; and depictions of feminist net art and new media art.</p>
-      <p>I worked with Mindy to develop an AR App for performative reading</p>
+  <>
+    <Collapse>
+      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/7VI57Zwf8QA?start=814" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      <p><a href="https://tour.cyberfeminismindex.com/" target='_blank' referrer='noreferrer'>Visit the Site</a></p>
+      <p>I worked with Mindy to create a desktop augmented reality application for live presentations of the Cyberfeminism Index. Performances with the application have been presented by New Museum with Rhizome (<a href="https://rhizome.org/editorial/2022/nov/30/the-power-of-spreadsheets-watch-the-cyberfeminism-index-book-launch/" target="_blank" referrer="noreferrer">documentation available here</a>), The Graham Foundation (<a href="https://youtu.be/5Nfd6Rah92E?t=1069" target="_blank" referrer="noreferrer">documentation available here</a>), and the Hammer Museum (<a href="https://channel.hammer.ucla.edu/video/1795/cyberfeminism-index-nora-khan-lauren-lee-mccarthy-mindy-seu" target="_blank" referrer="noreferrer">documentation available here</a>) among others.</p>
+      <p>CYBERFEMINISM INDEX</p>
+      <p>by Mindy Seu</p>
+      <p>The internet is not only a network of cables, servers, and computers. It is an environment that shapes and is shaped by its inhabitants and their use. Edited by designer, professor, and researcher Mindy Seu, CYBERFEMINISM INDEX includes more than 700 short entries of radical techno-critical activism, academic articles, hackerspaces, software education, net art, and more. Both a vital introduction for laypeople and a robust resource guide for educators, CYBERFEMINISM INDEX—an anti-canon, of sorts—celebrates and makes visible cyberfeminism’s long-ignored origins and its expansive legacy.</p>
+      <p>CYBERFEMINISM INDEX is published by Inventory Press and distributed by Distributed Art Publishers and generously funded by the Graham Foundation for Advanced Studies in the Fine Arts, Rhizome, and Feral File, and is made possible in part by the Rutgers University Research Council, Pratt Institute, Pioneer Works, and Cita Press.</p>
     </Collapse>
-      </>
+  </>
   );
 }
 export default CFI;
--- src/pages/systems.js
+++ src/pages/systems.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PUZZLE from './kuo-puzzle';
 import CFI from './seu-cfi';
-
+import BLUE from './artist-blue';
 const Systems = () =>{
   return (
     <article>
       <h1>Systems</h1>
-      <p>Collaborative and Technological Consulting, Web Programming, Physical Computing and Systems Design. From 2010 to now. Click for more info.</p>
+      <p>Since 2010 I have worked closely with artists to create one of a kind interactive systems for performances and installation. </p>
       <ul>
         <li><PUZZLE/></li>
         <li><CFI/></li>
+        <li><BLUE/></li>
       </ul>
     </article>
   );
