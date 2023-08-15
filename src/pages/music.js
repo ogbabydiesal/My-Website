864780d64ffd42ebf1db5e332bfeb226007ac367
tommy martinez <thomasjohnmartinez@gmail.com>
2023-08-14 22:42:35 -0400

    fixed integrity protocol embed width

--- src/pages/music.js
+++ src/pages/music.js
@@ -12,7 +12,7 @@ const Music = () =>{
       <section>
       <h3>Integrity Protocol / Lower Limb Lecture in collaboration with American Artist. <a href="https://www.ninaprotocol.com/8y9QheM5iKGT8ka9z1ZrTzQuhRqXQL77q2kt4kAcu169" target="_blank" rel="noreferrer">Stream and Collect on Nina Protocol</a>
       </h3>
-      <iframe src="https://dev.ninaprotocol.com/embed/release/8y9QheM5iKGT8ka9z1ZrTzQuhRqXQL77q2kt4kAcu169" width="100%" height="450px" frameborder="0" />
+      <iframe src="https://dev.ninaprotocol.com/embed/release/8y9QheM5iKGT8ka9z1ZrTzQuhRqXQL77q2kt4kAcu169" width="400px" height="400px" frameborder="0" />
       </section>
       <section>
       <h3>original mix on <a href="https://www.mixcloud.com/otherdesertradio/tommy-martinez-special-set-august-22-2022/" target="_blank" rel="noreferrer">Other Desert Radio</a>
