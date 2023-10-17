2a31cbdcc316880a83004ad0c40d714c001df31c
tommy martinez <thomasjohnmartinez@gmail.com>
2023-10-17 00:07:14 -0400

    added images

---
+++ src/pages/systems copy.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PUZZLE from './kuo-puzzle';
+import CFI from './seu-cfi';
+import BLUE from './artist-blue';
+import GOGGLES from './bennani-goggles';
+import MYTHICC from './syms-mythiccbeing';
+import BLOBS from './meisenberg-blobs';
+import VOMITORIUM from './vomitorium';
+
+const Systems = () =>{
+  return (
+    <article>
+      <h1>Systems</h1>
+      <p>Since 2010 I have worked closely with artists to create one of a kind interactive systems for performances and installation. </p>
+      <ul>
+        <li><PUZZLE/></li>
+        <li><CFI/></li>
+        <li><BLUE/></li>
+        <li><GOGGLES/></li>
+        <li><MYTHICC/></li>
+        <li><BLOBS/></li>
+        <li><VOMITORIUM/></li>
+      </ul>
+    </article>
+  );
+}
+export default Systems;
--- src/pages/systems.js
+++ src/pages/systems.js
@@ -11,16 +11,11 @@ const Systems = () =>{
   return (
     <article>
       <h1>Systems</h1>
-      <p>Since 2010 I have worked closely with artists to create one of a kind interactive systems for performances and installation. </p>
-      <ul>
-        <li><PUZZLE/></li>
-        <li><CFI/></li>
-        <li><BLUE/></li>
-        <li><GOGGLES/></li>
-        <li><MYTHICC/></li>
-        <li><BLOBS/></li>
-        
-      </ul>
+      <div class="grid grid-cols-4 gap-4">
+        <div>01</div>
+
+        <div>09</div>
+      </div>
     </article>
   );
 }
