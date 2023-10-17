c23f3261eb1f205e1e663fa3a69301ccea8a028a
tommy martinez <thomasjohnmartinez@gmail.com>
2023-10-17 01:53:53 -0400

    tiles test

--- src/pages/syms-mythiccbeing.js
+++ src/pages/syms-mythiccbeing.js
@@ -1,30 +1,12 @@
 import React, { useState } from 'react';
-import useCollapse from "react-collapsed";
 import { InstagramEmbed } from 'react-social-media-embed';
 
 
-function Collapse(props) {
-  const [isExpanded, setExpanded] = useState(true);
-  const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
+export default function MYTHICC({ onClose }) {
   return (
-    <div className="collapsible">
-      <h4 className="m-0 not-prose" {...getToggleProps({
-          onClick: () => setExpanded((prevExpanded) => !prevExpanded),
-        })}>
-          {isExpanded ? 'Martine Syms: Mythiccbbeing (2018)': 'Martine Syms: Mythiccbbeing (2018)'}
-      </h4>
-      <div {...getCollapseProps()}>
-        <div className="my-6">
-            {props.children}
-        </div>
-      </div>
-    </div>
-    );
-  }
-const MYTHICC = () =>{
-  return (
-   <>
-   <Collapse>
+    <div class="modal">
+      <div>
+      <button class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900" onClick={onClose}>Close</button>
       <p>Max, Chatbot, Multichannel Video, Sockets</p>
       <p><a href="https://www.bridgetdonahue.nyc/exhibitions/martine-syms/" target='_blank' referrer='noreferrer'>Visit the project at Bridget Donahue Gallery</a></p>
       <p>Mythiccbbeing was first produced for Martine's second solo show at Bridget Donahue Gallery in 2018. The technical aspect of the installation consisted of a multichannel audio visual playback triggered via interaction with an sms (text-message) based chatbot named Teeny. I oversaw all technical systems and acted as the sole programmer for interactive video playback engine. Angeline Meitzler acted as chatbot programmer.</p>
@@ -64,10 +46,9 @@ const MYTHICC = () =>{
         <li><a href="https://contemporaryand.com/exhibition/martine-syms-big-surprise/" target="_blank" referrer="noreferrer">C & 10</a></li>
         <li><a href="https://artfacts.net/exhibition/martine-syms-big-surprise/905195" target="_blank" referrer="noreferrer">Art Facts</a></li>
       </ul>
-    </Collapse>
-    <br></br>
-    </>
+      </div>
+      
+    </div>
   );
 }
-export default MYTHICC;
 
--- src/pages/systems.js
+++ src/pages/systems.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import PUZZLE from './kuo-puzzle';
 import CFI from './seu-cfi';
 import BLUE from './artist-blue';
@@ -7,16 +9,30 @@ import MYTHICC from './syms-mythiccbeing';
 import BLOBS from './meisenberg-blobs';
 import VOMITORIUM from './vomitorium';
 
-const Systems = () =>{
-  return (
-    <article>
-      <h1>Systems</h1>
-      <div class="grid grid-cols-4 gap-4">
-        <div>01</div>
 
-        <div>09</div>
-      </div>
-    </article>
+export default function SYSTEMS() {
+  const modals = [];
+  
+  const [showMythiccModal, setShowMythiccModal] = useState(false);
+  return (
+    <>
+      <article >
+        <h1>Systems</h1>
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+          <div><img src={require('../images/mythicc3.jpg')} alt="" onClick={() => setShowMythiccModal(true)} />
+          {showMythiccModal && createPortal(
+        <MYTHICC onClose={() => setShowMythiccModal(false)} />,
+        document.body
+      )}</div>
+          <div><img src={require('../images/delivery1.jpeg')} alt="" /></div>
+          <div><img src={require('../images/goggles.jpeg')} alt="" /></div>
+          <div><img src={require('../images/CFI.webp')} alt="" /></div>
+          <div><img src={require('../images/bls.jpg')} alt="" /></div>
+          <div><img src={require('../images/puzzle.png')} alt="" /></div>
+        </div>
+      </article>
+    </>
   );
 }
-export default Systems;
+
+
