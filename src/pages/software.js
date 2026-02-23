dea6aa6954785958e0cf6f51dadfadbd55e127f2
tommy martinez <thomasjohnmartinez@gmail.com>
2026-02-23 18:25:22 -0500

    removed unnescessary files

--- src/pages/software.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-const Software = () =>{
-  return (
-    <div className="main">
-      <p>select a software project from the nav</p>
-    </div>
-  );
-}
-export default Software;
